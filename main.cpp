9e68ee28e5095365e8729c9fc65d7a1641061e49
TuanTran <jangle@students.highline.edu>
2022-03-04 20:25:46 -0800

    step2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
 
-int main() 
+class Shape
+{    
+};
+
+class Rectangle: public Shape
 {
-  std::cout << "Hello, World" << std::endl;
-  return 0;
+    public:
+    Rectangle(double width, double height)
+    : width{width}, height{height} {
+    }
+
+    private:
+    double width;
+    double height;
+};
+
+class Square: public Shape {
+    public:
+    Square(double side)
+    : side(side) {        
+    }
+
+    private:
+    double side;
+};
+
+class Circle: public Shape {
+    public:
+    Circle(double radius)
+    : radius{radius} {
+    }
+
+    private:
+    double radius;
+};
+
+int main() {
+    Rectangle r1{ 3.0, 5.0 };
+    Square s1{ 4.0 };
+    Circle c1{ 10.0 };
+
+    // printArea(r1);
+    // printArea(s1);
+    // printArea(c1);
 }
